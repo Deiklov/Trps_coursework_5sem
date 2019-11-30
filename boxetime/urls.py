4a6b4969c1ebcf9e40f8fd127ea9a9f0dc130c49
Deiklov <romanov408g@mail.ru>
2019-11-30 20:06:31 +0300

    change popper.js

--- boxetime/urls.py
+++ boxetime/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from django.views.generic import TemplateView
+from datetime import *
 
 urlpatterns = [
     path('', views.main, name="main"),
@@ -9,10 +10,10 @@ urlpatterns = [
     path('event/<int:number>', views.event, name="concrete_event"),
     path('cabinet', TemplateView.as_view(template_name="cabinet.html"), name="cabinet"),
     path('instruction', TemplateView.as_view(template_name="instruction.html"), name="instruction"),
-    path('search', TemplateView.as_view(template_name="search_template.html"), name="temp_search"),
     path('event_list', views.event_list, name="event_list"),
     path('add_request/', views.addrequest, name="add_request"),
     path('req_handler/<int:req_id>/<int:flag>', views.req_handler, name="req_handler"),
     path('update_grid/<int:eventid>', views.update_grid, name="update_grid"),
+    path('search', views.search, name="search"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
